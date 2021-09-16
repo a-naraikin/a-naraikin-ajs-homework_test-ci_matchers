673f425d982f879d153dd89c04e249879db92066
a-naraikin <anna.naraikin@yandex.ru>
2021-09-16 10:31:49 +0300

    fix: task: matchers

--- src/js/__tests__/sort-by-health.test.js
+++ src/js/__tests__/sort-by-health.test.js
@@ -18,6 +18,22 @@ test('should return the heroes in descending order of health', () => {
 
 test('should return the heroes in descending order of health', () => {
   const result = sortHealth([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
+
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(result).not.toBe(expected);
+});
+
+test('should return the heroes in descending order of health', () => {
+  const result = sortHealth([
     { name: 'мечник', health: 80 },
     { name: 'маг', health: 80 },
     { name: 'лучник', health: 80 },
